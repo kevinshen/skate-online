5ad171ec5a69a7e02b7203a115c5e682dcc19e2b
kevinshen <kevin.shen@columbia.edu>
2016-07-19 21:08:06 -0400

    Set up

---
+++ SkateOnline/SkateViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SkateViewController.swift
+//  SkateOnline
+//
+//  Created by Kevin Shen on 7/19/16.
+//  Copyright © 2016 Kevin Shen. All rights reserved.
+//
+
+import UIKit
+
+class SkateViewController: UIViewController {
+
+}

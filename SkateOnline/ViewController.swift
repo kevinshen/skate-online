372f5a8e026f607cc6338a352611b4dd2736034c
kevinshen <kevin.shen@columbia.edu>
2016-07-28 21:56:01 -0400

    Add screen name text field to homepage

--- SkateOnline/ViewController.swift
+++ SkateOnline/ViewController.swift
@@ -8,17 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var validNameLabel: UILabel!
+    @IBOutlet var screenNameTextField: UITextField!
+    @IBOutlet var playNowButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        validNameLabel.text = ""
+        
+        screenNameTextField.delegate = self;
+        screenNameTextField.text = "Screen Name"
+        screenNameTextField.textColor = UIColor.lightGrayColor()
+        
+        playNowButton.userInteractionEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.textColor == UIColor.lightGrayColor() {
+            textField.text = nil
+            textField.textColor = UIColor.blackColor()
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+        if textField.text == "Kevin" {
+            validNameLabel.text = "Welcome mate"
+            playNowButton.userInteractionEnabled = true
+        } else if textField.text!.isEmpty {
+            validNameLabel.text = ""
+            textField.text = "Screen Name"
+            textField.textColor = UIColor.lightGrayColor()
+            playNowButton.userInteractionEnabled = false
+        } else {
+            validNameLabel.text = "Nice scooter n00b"
+            playNowButton.userInteractionEnabled = false
+        }
+        
+    }
+
 
 
 }
